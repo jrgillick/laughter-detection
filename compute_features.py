4247283eee3e8d2582b97d2ea409b91103496835
Jon Gillick <jrgillick@gmail.com>
2017-05-29 09:44:43 -0700

    Added segmenter, command line interface and stored models

---
+++ compute_features.py
@@ -0,0 +1,328 @@
+import numpy as np
+import pandas as pd
+import librosa
+import IPython.display
+import os
+import sys
+import audioread
+from python_speech_features import mfcc
+from python_speech_features import delta
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+import pickle
+
+# methods for getting files from Switchboard Corpus
+def get_train_val_test_folders(t_root):
+    t_folders = [t_root + f for f in os.listdir(t_root) if os.path.isdir(t_root + f)]
+    t_folders.sort()
+    train_folders = t_folders[0:20]
+    val_folders = t_folders[20:25]
+    test_folders = t_folders[25:30]
+    train_folders.sort(); val_folders.sort(); test_folders.sort()
+    return (train_folders, val_folders, test_folders)
+
+def get_transcriptions_files(folder):
+    files = []
+    subfolders = [folder + "/" + f for f in os.listdir(folder)]
+    for f in subfolders:
+        fs = [f + "/" + fname for fname in os.listdir(f) if 'a-word.text' in fname and 'A' in fname]
+        files += fs
+    files.sort()
+    return files
+
+def get_all_transcriptions_files(folder_list):
+    files = []
+    for folder in folder_list:
+        files += get_transcriptions_files(folder)
+    files.sort()
+    return files
+
+def get_transcription_files_with_laughter_in_corpus(folder_list):
+    files = []
+    transcription_files = get_all_transcriptions_files(folder_list)
+    for f in transcription_files:
+        if count_laughter_instances_in_transcription_file(f) > 0:
+            files.append(f)
+    return files
+
+def count_transcription_files_with_laughter_in_corpus(folder_list):
+    return len(get_transcription_files_with_laughter_in_corpus(folder_list))
+
+def get_sph_files(folder):
+    return [folder + "/" + f for f in os.listdir(folder) if ".sph" in f]
+
+def get_all_audio_files(a_root):
+    files = []
+    a_folders = [a_root + f + "/data" for f in os.listdir(a_root) if os.path.isdir(a_root + f)]
+    a_folders.sort()
+    for folder in a_folders:
+        files += get_sph_files(folder)
+    files.sort()
+    return files
+
+def get_text_from_file(f):
+    return (open(f).read().split("\n"))[0:-1]
+
+def get_laughter_rows_from_file(f):
+    return [l for l in get_text_from_file(f) if 'laughter' in l]
+    #return [l for l in get_text_from_file(f) if '[laughter]' in l] # doesn't allow laughter with words together
+
+def get_audio_file_from_id(d):
+    files = [f for f in all_audio_files if d in f]
+    if len(files) == 1:
+        return files[0]
+    elif len(files) > 1:
+        print "Warning: More than 1 audio file matched id %d" % (int(d))
+        return None
+    else:
+        print "Warning: No audio file matched id %d" % (int(d))
+        return None
+        
+def get_id_from_row(row):
+    return row[2:6]
+
+def get_id_from_file(f):
+    return get_id_from_row(get_text_from_file(f)[0])
+
+def get_audio_file_from_row(row):
+    return get_audio_file_from_id(get_id_from_row(row))
+
+def get_audio_file_from_transcription_text(t):
+    return get_audio_file_from_id(get_id_from_row(t[0]))
+
+def get_audio_file_from_transcription_file(f):
+    t = open(f).read().split('\n')
+    return get_audio_file_from_id(get_id_from_row(t[0]))
+
+def get_audio_file_length(path):
+    f = audioread.audio_open(path)
+    l = f.duration
+    f.close()
+    return l
+
+def count_laughter_instances_in_transcription_file(f):
+    rows = get_laughter_rows_from_file(f)
+    return len(rows)
+
+def count_laughter_instances_in_corpus(folder_list):
+    transcription_files = get_all_transcriptions_files(folder_list)
+    count = 0
+    for f in transcription_files:
+        count += count_laughter_instances_in_transcription_file(f)
+    return count
+
+def get_audio_files_from_transcription_files(transcription_files):
+    files = []
+    for f in transcription_files:
+        files.append(get_audio_file_from_transcription_file(f))
+    files = list(set(files))
+    files.sort()
+    if None in files: files.remove(None)
+    return files
+
+def extract_times_from_row(row):
+    return (float(row.split(' ')[1]), float(row.split(' ')[2]))
+
+def get_laughter_regions_from_file(t_file):
+    rows = get_laughter_rows_from_file(t_file)
+    times = []
+    for row in rows:
+        start, end = extract_times_from_row(row)
+        if end - start > 0.05:
+            times.append((start,end))
+    return times
+
+def get_length_from_regions_list(times):
+    return sum([end - start for start, end in times])
+    
+def get_random_speech_region_from_file(t_file, region_length):
+    audio_length = get_audio_file_length(get_audio_file_from_transcription_file(t_file))
+    contains_laughter = True
+    tries = 0
+    while(contains_laughter):
+        tries += 1
+        if tries > 10:
+            print "audio length %f" % (audio_length)
+            print "region legnth %f" % (region_length)
+            return None
+        start = np.random.uniform(1.0, audio_length - region_length - 1.0)
+        end = start + region_length
+        if no_laughter_present(t_file,start,end):
+            contains_laughter = False
+    return (start, end)
+
+# Check if laughter is present in a region of an audio file by looking at the transcription file
+def no_laughter_present(t_file,start,end):
+    all_rows = get_text_from_file(t_file)
+    for row in all_rows:
+        region_start, region_end = extract_times_from_row(row)
+        if times_overlap(float(region_start), float(region_end), float(start), float(end)):
+            if 'laughter' in row.split(' ')[-1]:
+                return False
+    return True
+        
+def times_overlap(start1, end1, start2, end2):
+    if end1 < start2 or end2 < start1:
+        return False
+    else:
+        return True
+
+
+
+
+
+#Methods for processing audio and computing MFCC and Delta features
+
+# pad with 0.5 seconds on each side of the desired region
+def clip_audio_region(y,sr,start,end,pad_amount=0.5):
+    start_sample = int((start-pad_amount)*sr)
+    end_sample = int((end+pad_amount)*sr)
+    return y[start_sample:end_sample]
+
+def write_clip_to_disk(path,y,sr):
+    librosa.output.write_wav(path,y,sr)
+    
+def compute_mfcc_features(y,sr):
+    return mfcc(y,samplerate=sr,winlen=0.025,winstep=0.01)
+
+def compute_delta_features(mfcc_feat):
+    return delta(mfcc_feat, 2)
+
+def compute_labels_per_frame(n_frames,sr,winstep=0.01,pad_amount=0.5):
+    #print "n_frames: %d" % (n_frames)
+    samples_per_frame = sr*winstep #80 with defaults
+    #with 0.5 seconds of padding, there should be 4000 samples of padding, so 50 frames of non-laughter 
+    n_padding_frames = int(sr * pad_amount / samples_per_frame)
+    padding_frames = list(np.zeros(n_padding_frames))
+    laughter_frames = list(np.ones(n_frames - 2*n_padding_frames))
+    labels = padding_frames + laughter_frames + padding_frames
+    return labels
+
+def compute_features_and_labels(y,sr,region,label_type,source_file_id,file_index):
+    clip = clip_audio_region(y,sr,start=region[0],end=region[1])
+    mfcc_features = compute_mfcc_features(clip,sr)
+    delta_features = compute_delta_features(mfcc_features)
+    n_frames = len(mfcc_features)
+    if label_type == 'laughter':
+        labels = compute_labels_per_frame(n_frames,sr)
+    else:
+        labels = np.zeros(n_frames)
+    return {'mfcc': mfcc_features,
+            'delta': delta_features,
+            'labels': labels,
+            'clip_type': label_type,
+            'source_file_id': source_file_id,
+            'file_index': file_index}
+
+def compute_and_store_features_and_labels(t_file, output_dir):
+    a_file = get_audio_file_from_transcription_file(t_file)
+    y,sr = librosa.load(a_file,sr=8000)
+    source_file_id = get_id_from_file(t_file)
+    laughter_regions = get_laughter_regions_from_file(t_file)
+    
+    laughter_features_list = [compute_features_and_labels(y,sr,region,label_type='laughter',source_file_id=source_file_id,file_index=index) for index, region in enumerate(laughter_regions)]
+    speech_regions = [get_random_speech_region_from_file(t_file, get_length_from_regions_list(laughter_regions)) for i in xrange(3)]
+    speech_features_list = [compute_features_and_labels(y,sr,region,label_type='speech',source_file_id=source_file_id,file_index=index) for index, region in enumerate(speech_regions)]
+    
+    laughter_output_file = output_dir + "laughter_" + source_file_id + ".pkl"
+    speech_output_file = output_dir + "speech_" + source_file_id + ".pkl"
+    
+    with open(laughter_output_file, "wb") as f:
+        pickle.dump(laughter_features_list, f)
+
+    with open(speech_output_file, "wb") as f:
+        pickle.dump(speech_features_list, f)
+
+def compute_all_features(transcription_file_list, output_dir):
+    for index, t_file in enumerate(transcription_file_list):
+        print "Processing %d out of %d transcription files." % (index+1, len(transcription_file_list))
+        try:
+            compute_and_store_features_and_labels(t_file, output_dir)
+        except:
+						print "File %d Failed" % (index + 1)
+
+def parse_inputs():
+	process = True
+
+	try:
+		t_root = sys.argv[1]
+	except:
+		print "Enter the switchboard transcriptions root dir as the first argument"
+		process = False
+
+	try:
+		a_root = sys.argv[2]
+	except:
+		print "Enter the switchboard audio root directory as the second argument"
+		process = False
+
+	try:
+		train_output_dir = sys.argv[3]
+	except:
+		print "Enter the training set output directory as the third argument"
+		process = False
+	
+	try:
+		validation_output_dir = sys.argv[4]
+	except:
+		print "Enter the validation set output directory as the fourth argument"
+		process = False
+
+	try:
+		test_output_dir = sys.argv[5]
+	except:
+		print "Enter the test set output directory as the fourth argument"
+		process = False
+
+	if process:
+		return (t_root, a_root, train_output_dir, validation_output_dir, test_output_dir)
+	else:
+		return False
+
+
+
+# Usage: python compute_features.py <switchboard_transcriptions_dir> <switchboard_audio_dir> <train_output_dir> <val_output_dir> <test_output_dir>
+
+if __name__ == '__main__':
+	if parse_inputs():
+		t_root, a_root, train_output_dir, validation_output_dir, test_output_dir = parse_inputs()
+	
+		# Get transcriptions root dir
+
+		all_audio_files = get_all_audio_files(a_root)
+		train_folders, val_folders, test_folders = get_train_val_test_folders(t_root)
+
+		print "Laughter instances in training data: %d" % (count_laughter_instances_in_corpus(train_folders))
+		print "Laughter instances in validation data: %d" % (count_laughter_instances_in_corpus(val_folders))
+		print "Laughter instances in test data: %d" % ( count_laughter_instances_in_corpus(test_folders))
+		print
+		print "Files containing laughter in training data: %d" % (count_transcription_files_with_laughter_in_corpus(train_folders))
+		print "Files containing laughter in validation data: %d" % (count_transcription_files_with_laughter_in_corpus(val_folders))
+		print "Files containing laughter in test data: %d" % (count_transcription_files_with_laughter_in_corpus(test_folders))
+		print
+
+		train_audio_files = get_audio_files_from_transcription_files(get_all_transcriptions_files(train_folders))
+		val_audio_files = get_audio_files_from_transcription_files(get_all_transcriptions_files(val_folders))
+		test_audio_files = get_audio_files_from_transcription_files(get_all_transcriptions_files(test_folders))
+
+		train_transcription_files = get_transcription_files_with_laughter_in_corpus(train_folders)
+		val_transcription_files = get_transcription_files_with_laughter_in_corpus(val_folders)
+		test_transcription_files = get_transcription_files_with_laughter_in_corpus(test_folders)
+
+		train_audio_files = get_audio_files_from_transcription_files(train_transcription_files)
+		val_audio_files = get_audio_files_from_transcription_files(val_transcription_files)
+		test_audio_files = get_audio_files_from_transcription_files(test_transcription_files)
+		print
+		print "Training on %d dialogues" % len(train_audio_files)
+		print "Validatiing on %d dialogues" % len(val_audio_files)
+		print "Testing on %d dialogues" % len(test_audio_files)
+
+		
+		print "Computing Features for Training Data..."
+		compute_all_features(train_transcription_files, train_output_dir)
+
+		print "Computing Features for Validation Data..."
+		compute_all_features(val_transcription_files, validation_output_dir)
+
+		print "Computing Features for Test Data..."
+		compute_all_features(test_transcription_files, test_output_dir)
---
+++ segment_laughter.py
@@ -0,0 +1,158 @@
+import numpy as np
+import scipy.signal as signal
+import pickle
+import os
+from sklearn.utils import shuffle
+import sys
+import librosa
+import IPython.display
+from python_speech_features import mfcc
+from python_speech_features import delta
+import argparse
+
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Flatten, Dropout
+import keras.optimizers
+from keras.models import load_model
+import keras.regularizers
+from keras.regularizers import l2, l1
+
+import compute_features
+
+def frame_to_time(frame_index):
+	return(frame/100.)
+
+def seconds_to_frames(s):
+	return(int(s*100))
+
+def collapse_to_start_and_end_frame(instance_list):
+    return (instance_list[0], instance_list[-1])
+
+def frame_span_to_time_span(frame_span):
+    return (frame_span[0] / 100., frame_span[1] / 100.)
+
+def seconds_to_samples(s,sr):
+    return s*sr
+
+def format_features(mfcc_feat, delta_feat,index):
+    return np.append(mfcc_feat[index-window_size:index+window_size],delta_feat[index-window_size:index+window_size])
+
+def cut_laughter_segments(instance_list,y,sr):
+	new_audio = []
+	for start, end in instance_list:
+		sample_start = int(seconds_to_samples(start,sr))
+		sample_end = int(seconds_to_samples(end,sr))
+		clip = y[sample_start:sample_end]
+		new_audio = np.concatenate([new_audio,clip])
+	return new_audio
+
+def get_instances_from_rows(rows):
+	return [(float(row.split(' ')[1]),float(row.split(' ')[2])) for row in rows]
+
+def lowpass(sig, filter_order = 2, cutoff = 0.01):
+	#Set up Butterworth filter
+	filter_order  = 2 
+	B, A = signal.butter(filter_order, cutoff, output='ba')
+ 
+	#Apply the filter
+	return(signal.filtfilt(B,A, sig))
+
+def get_laughter_instances(probs, threshold = 0.5, min_length = 20):
+	instances = []
+	current_list = []
+	for i in xrange(len(probs)):
+		if np.min(probs[i:i+1]) > threshold:
+			current_list.append(i)
+		else:
+			if len(current_list) > 0:
+				instances.append(current_list)
+				current_list = []
+
+	instances = [frame_span_to_time_span(collapse_to_start_and_end_frame(i)) for i in instances if len(i) > min_length]
+	return instances
+
+def parse_inputs():
+	process = True
+
+	try:
+		a_file = sys.argv[1]
+	except:
+		print "Enter the audio file path as the first argument"
+		process = False
+
+	try:
+		model_path = sys.argv[2]
+	except:
+		print "Enter the stored model path as the second argument"
+		process = False
+
+	try:
+		output_audio_path = sys.argv[3]
+	except:
+		print "Enter the output audio path as the third argument"
+		process = False
+
+	try:
+		threshold = float(sys.argv[4])
+	except:
+		threshold= 0.5
+
+	try:
+		min_length = float(sys.argv[5])
+	except:
+		min_length = 0.2
+
+	if process:
+		return (a_file, model_path, output_audio_path, threshold, min_length)
+	else:
+		return False
+
+
+
+
+# Usage: python segment_laughter.py <input_audio_file> <stored_model_path> <output_audio_path>
+
+if __name__ == '__main__':
+	if parse_inputs():
+		a_file, model_path, output_audio_path, threshold, min_length = parse_inputs()
+		min_length = seconds_to_frames(min_length)
+
+		print
+		print "Loading audio file..."
+		y,sr = librosa.load(a_file,sr=8000)
+		full_res_y, full_res_sr = librosa.load(a_file,sr=44100)
+
+		model = load_model(model_path)
+		window_size = 37
+	
+		mfcc_feat = compute_features.compute_mfcc_features(y,sr)
+		delta_feat = compute_features.compute_delta_features(mfcc_feat)
+		
+		zero_pad = np.zeros((window_size,mfcc_feat.shape[1]))
+		padded_mfcc_feat = np.vstack([zero_pad,mfcc_feat,zero_pad])
+		padded_delta_feat = np.vstack([zero_pad,delta_feat,zero_pad])
+
+		feature_list = []
+		for i in range(window_size, len(mfcc_feat) + window_size):
+			feature_list.append(format_features(padded_mfcc_feat, padded_delta_feat, i))
+		feature_list = np.array(feature_list)
+		
+		print
+		print "Looking for laughter..."
+		probs = model.predict_proba(feature_list).reshape((len(mfcc_feat),))
+		filtered = lowpass(probs)
+
+		instances = get_laughter_instances(filtered, threshold=threshold, min_length=min_length)
+
+		laughs = cut_laughter_segments(instances,full_res_y,full_res_sr)
+		librosa.output.write_wav(output_audio_path, laughs, full_res_sr)
+
+		print
+		print
+		print "Wrote laughter to file: %s" % (output_audio_path)
+		print
+		print "Laughter Segments: "
+		print instances
+		print

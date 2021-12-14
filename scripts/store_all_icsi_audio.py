ead1effb51b8542b2bbc797077dadf77f6072d77
Lasse Wolter <wolter.lasse@gmail.com>
2021-12-14 16:19:26 +0000

    [scripts] First version of icsi store_data script

--- scripts/store_all_icsi_audio.py
+++ scripts/store_all_icsi_audio.py
@@ -1,6 +1,6 @@
 # Usage
-# `python store_all_switchboard_audio.py`
-# This assumes you'll have downloaded switchboard data into a specific location.
+# `python store_all_icsi_audio.py`
+# This assumes you'll have downloaded icsi data into a specific location.
 # And also assumes you'll be (outputting) pre-processed data into another specific location.
 # If you want to change the paths, adjust them in the strings below.
 
@@ -17,66 +17,66 @@ import data_loaders
 import audio_utils
 import dataset_utils
 
-# switchboard_audio_path
-a_root = '../data/switchboard/switchboard-1/97S62/'
+# icsi_audio_path
+a_root = '../data/icsi/Signals'
 
-# switchboard_transcriptions_path
-t_root = '../data/switchboard/switchboard-1/swb_ms98_transcriptions/'
+# icsi_transcriptions_path
+t_root = '../data/icsi/transcripts/'
 
+# icsi sample rate
+sample_rate = 16000
 
 all_audio_files = librosa.util.find_files(a_root, ext='sph')
 train_folders, val_folders, test_folders = dataset_utils.get_train_val_test_folders(
-    t_root)
+    a_root)
 
-train_transcription_files_A, train_audio_files = dataset_utils.get_audio_files_from_transcription_files(
-    dataset_utils.get_all_transcriptions_files(train_folders, 'A'), all_audio_files)
-train_transcription_files_B, _ = dataset_utils.get_audio_files_from_transcription_files(
-    dataset_utils.get_all_transcriptions_files(train_folders, 'B'), all_audio_files)
+# Get audio files for each split
+train_audio_files = [f for dir in train_folders for f in os.listdir(dir)]
+val_audio_files = [f for dir in val_folders for f in os.listdir(dir)]
+test_audio_files = [f for dir in test_folders for f in os.listdir(dir)]
 
-val_transcription_files_A, val_audio_files = dataset_utils.get_audio_files_from_transcription_files(
-    dataset_utils.get_all_transcriptions_files(val_folders, 'A'), all_audio_files)
-val_transcription_files_B, _ = dataset_utils.get_audio_files_from_transcription_files(
-    dataset_utils.get_all_transcriptions_files(val_folders, 'B'), all_audio_files)
 
-test_transcription_files_A, test_audio_files = dataset_utils.get_audio_files_from_transcription_files(
-    dataset_utils.get_all_transcriptions_files(test_folders, 'A'), all_audio_files)
-test_transcription_files_B, _ = dataset_utils.get_audio_files_from_transcription_files(
-    dataset_utils.get_all_transcriptions_files(test_folders, 'B'), all_audio_files)
-
+# CREATE TRAINING HASHMAP
 h = {}
 train_y = audio_utils.parallel_load_audio_batch(
-    train_audio_files, n_processes=8, sr=8000)
+    train_audio_files, n_processes=8, sr=sample_rate)
+# Make sure all files have been loaded
 assert(len(train_y) == len(train_audio_files))
 for i in range(len(train_audio_files)):
     f = train_audio_files[i]
     y = train_y[i]
     h[f] = y
 
-with open("../data/switchboard/train/swb_train_audios1.pkl", "wb") as f:
+with open("../data/icsi/train/swb_train_audios1.pkl", "wb") as f:
     pickle.dump(h, f)
 
 
+# CREATE VALIDATION HASHMAP
 h = {}
 val_y = audio_utils.parallel_load_audio_batch(
-    val_audio_files, n_processes=8, sr=8000)
-assert(len(val_y) == len(val_audio_files))
+    val_audio_files, n_processes=8, sr=sample_rate)
+# Make sure all files have been loaded
+assert(len(val_y) == len(val_audio_files)) 
+
 for i in range(len(val_audio_files)):
     f = val_audio_files[i]
     y = val_y[i]
     h[f] = y
 
-with open("../data/switchboard/val/swb_val_audios1.pkl", "wb") as f:
+with open("../data/icsi/val/swb_val_audios1.pkl", "wb") as f:
     pickle.dump(h, f)
 
 
+# CREATE TEST HASHMAP
 h = {}
 test_y = audio_utils.parallel_load_audio_batch(
-    test_audio_files, n_processes=8, sr=8000)
+    test_audio_files, n_processes=8, sr=sample_rate)
+# Make sure all files have been loaded
 assert(len(test_y) == len(test_audio_files))
 for i in range(len(test_audio_files)):
     f = test_audio_files[i]
     y = test_y[i]
     h[f] = y
 
-with open("../data/switchboard/test/swb_test_audios1.pkl", "wb") as f:
+with open("../data/icsi/test/swb_test_audios1.pkl", "wb") as f:
     pickle.dump(h, f)
